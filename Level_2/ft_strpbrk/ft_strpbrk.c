9ae066a289c8b76e402604c1a0c3ffa31d15b6e5
Hamid Oukaamouch <houkaamo@e2r11p4.1337.ma>
2026-01-03 19:05:53 +0100

    level_2: add ft_strpbrk solution

---
+++ Level_2/ft_strpbrk/ft_strpbrk.c
@@ -0,0 +1,18 @@
+#include <stddef.h>
+
+char	*ft_strpbrk(const char *s1, const char *s2)
+{
+	int i = 0;
+	while(s1[i])
+	{
+		int j = 0;
+		while(s2[j])
+		{
+			if(s1[i] == s2[j])
+				return (char *)(s1 + i);
+			j++;
+		}
+		i++;
+	}
+	return NULL;
+}
