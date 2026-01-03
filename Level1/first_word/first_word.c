58c488a219f6926cc88131cecce4687be3ff3f94
Hamid Oukaamouch <houkaamo@e2r11p4.1337.ma>
2026-01-03 11:08:54 +0100

    level1: add first_word

---
+++ Level1/first_word/first_word.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+
+int	is_space(char c)
+{
+	if(c == ' ' || (c >= 9 && c <= 13))
+		return 1;
+	return 0;
+}
+int main(int argc, char **argv)
+{
+	if(argc == 2)
+	{
+		int i = 0;
+		while(is_space(argv[1][i]))
+			i++;
+		while(argv[1][i] && !is_space(argv[1][i]))
+			write(1, &argv[1][i++], 1);
+
+	}
+	write(1, "\n", 1);
+}
