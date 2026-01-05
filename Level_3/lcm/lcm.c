649f63874b8f0674f98214edecfdf04323de4146
Hamid Oukaamouch <hamidkaamouch123@gmail.com>
2026-01-05 12:53:27 +0100

    level_3: add lcm solution

---
+++ Level_3/lcm/lcm.c
@@ -0,0 +1,17 @@
+
+unsigned int lcm(unsigned int a, unsigned int b)
+{
+	if(a == 0 || b == 0)
+		return 0;
+	unsigned int	i;
+	if(a > b)
+		i = a;
+	else
+		i = b;
+	while(1)
+	{
+		if((i % a == 0) && (i % b == 0))
+			return i;
+		i++;
+	}
+}
